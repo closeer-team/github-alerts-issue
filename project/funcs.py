d08be1bb65abecef0da8e7723407e1bdd8c3e2c2
jackson541 <jacksonalves541@gmail.com>
2022-12-08 19:51:03 -0300

    change code to check related PRs in issues

--- project/funcs.py
+++ project/funcs.py
@@ -2,77 +2,45 @@ from github import Github
 from .constants import *
 import requests
 import json
-import time
-import redis
-from urllib.parse import urlparse
+import re
 
 # using an access token
 g = Github(ACCESS_TOKEN)
 repo = g.get_repo(PROJECT_TO_TRACK)
 
-if REDIS_URL:
-    url = urlparse(REDIS_URL)
-    redis_var = redis.Redis(host=url.hostname, port=url.port, password=url.password)
-else:
-    redis_var = redis.Redis(host='localhost', port=6379, db=0)
 
-
-def check_pr_branch_is_correct(request_data: dict):
+def get_related_issue(pr_id: int):
     """
-    Check if branch destination of PR in webhook is same of BRANCH_TO_TRACK
+    Retorna a issue relacionada ao PR
 
-    - request_data [dict]: body of request to API
+    - pr_id [int]: ID do PR a ser verificado
+
     """
-    return request_data['pull_request']['base']['ref'] == BRANCH_TO_TRACK
-    
+    pr_id = str(pr_id)
+    issues = repo.get_issues(state='open', labels=['ajuste pr'])
+    related_issue = None
 
-def get_not_mergeable_prs():
+    for issue in issues:
+        pr_data = re.search("#\d+", issue.body)
+        if pr_data and pr_id in pr_data.group():
+            related_issue = issue.number
+            break
+
+    return related_issue
+
+
+def notify_related_issue_in_slack(issue_id: int, user_login: str, pr_id: int):
     """
-    Search for PRs with conflicts
-    """
-    not_mergeable_prs = []
+    Envia a notificação de issue relacionada ao PR para o slack
 
-    pulls = repo.get_pulls(state='open', sort='created', base=BRANCH_TO_TRACK)
-    for pr in pulls:
-        pr_detail = repo.get_pull(pr.number)
-        if pr_detail.mergeable == None:
-            """ After merge a PR into BRANCH_TO_TRACK, GitHub set all others PRs opens to it with mergeable = None.
-            So, is necessary send a request for this PRs and await some time to GitHub calculated again mergeable of they. """
-            time.sleep(2)
-            pr_detail = repo.get_pull(pr.number)
-
-        if not pr_detail.mergeable and pr_detail.mergeable != None: 
-            not_mergeable_prs.append(str(pr.number))
-
-    return not_mergeable_prs
-
-
-def check_there_are_new_not_mergeable_pr(not_mergeable_prs: list):
-    """
-    Check if in the list of PRs with conflicts there are any new PR
-
-    - not_mergeable_prs [array]: list of Pull Resquest IDs in string format
-    """
-    old_prs_in_byte = redis_var.get('prs_conflicts')
-    old_prs_with_conflicts = old_prs_in_byte.decode('utf-8').split(',') if old_prs_in_byte else list()
-
-    new_prs_in_conflict = list(set(not_mergeable_prs) - set(old_prs_with_conflicts))
-    if not new_prs_in_conflict:
-        return False
-    
-    concated_list_prs = ','.join(new_prs_in_conflict + old_prs_with_conflicts)
-    redis_var.setex('prs_conflicts', ONE_DAY_IN_SECONDS, bytes(concated_list_prs, encoding='utf-8'))
-    return True
-
-
-def notify_not_mergeable_prs_in_slack(not_mergeable_prs: list):
-    """
-    Send notification to slack with the IDs of PRs with conflicts
-
-    - not_mergeable_prs [array]: list of Pull Resquest IDs in string format
+    - issue_id [int]: ID da issue relacionada
+    - user_login [str]: Nome de usuário do responsável pelo PR
+    - pr_id [int]: ID do Pull Request
     """
     data = {
-        'prs_list': ', '.join(not_mergeable_prs)
+        'issue_id': str(issue_id),
+        'user_login': str(user_login),
+        'pr_id': str(pr_id),
     }
 
     requests.post(SLACK_WEBHOOK_LINK, data=json.dumps(data))
--- project/main.py
+++ project/main.py
@@ -1,30 +1,25 @@
 from flask import request, Response, Blueprint
 from .check_signature import verify_signature
-from .funcs import check_pr_branch_is_correct, get_not_mergeable_prs, notify_not_mergeable_prs_in_slack, check_there_are_new_not_mergeable_pr
+from .funcs import get_related_issue, notify_related_issue_in_slack
 import json
 
 bp = Blueprint('main', __name__, url_prefix='/')
 
-@bp.route('/check_conflicts', methods=['POST',])
+@bp.route('/check_pending_issues', methods=['POST',])
 def check_prs():
     signature_256 = request.headers['X-Hub-Signature-256']
     if not verify_signature(signature_256, request.data):
         return Response(status=403)
 
-    request_data = json.loads(request.data)
+    pr_data = json.loads(request.data)
 
-    if not check_pr_branch_is_correct(request_data):
-        return Response(status=200)
-
-    not_mergeable_prs = get_not_mergeable_prs()
-
-    if not check_there_are_new_not_mergeable_pr(not_mergeable_prs):
-        return Response(status=200)
-
-    if not_mergeable_prs:
-        notify_not_mergeable_prs_in_slack(not_mergeable_prs)
-    else:
-        print('all opens PRs are OK!')
+    related_issue = get_related_issue(pr_data['number'])
+    if related_issue:
+        notify_related_issue_in_slack(
+            related_issue,
+            pr_data['pull_request']['user']['login'],
+            pr_data['number']
+        )
 
     return Response(status=200)
 
